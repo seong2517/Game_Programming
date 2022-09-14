e833b4d5bd47a9cbfb037d63eabc50aa81667ef8
Jeong SeongHo <35947690+seong2517@users.noreply.github.com>
2022-09-14 16:41:49 +0900

    Create gugudan.c

---
+++ 0914/gugudan.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <windows.h>
+
+void gotoxy(int x, int y);
+
+int main(void)
+{
+	for (int i = 1; i <= 9; i++)
+	{
+		for (int j = 1; j <= 9; j++)
+		{
+			gotoxy(j * 10, i);
+			printf("%d*%d=%2d", i, j, i * j);
+		}
+	}
+	printf("\n");
+
+	return 0;
+}
+
+void gotoxy(int x, int y)
+{
+	COORD Pos = { x - 1, y - 1 };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
