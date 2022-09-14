bd69ba2f5a6a216622eb35e14853c7c553cfbc24
Jeong SeongHo <35947690+seong2517@users.noreply.github.com>
2022-09-14 16:48:55 +0900

    Update cls01.c

--- 0914/cls01.c
+++ 0914/cls01.c
@@ -3,11 +3,11 @@
 
 int main(void)
 {
-char ch;
-printf("문자를 입력하고 Enter>");
-scanf("%c", &ch);
-system("cls");
-printf("입력된 문자 %c\n", ch);
+  char ch;
+  printf("문자를 입력하고 Enter>");
+  scanf("%c", &ch);
+  system("cls");
+  printf("입력된 문자 %c\n", ch);
   
-return 0;
+  return 0;
 }
